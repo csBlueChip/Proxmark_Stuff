8167a92d3745bba94237bfb9841c328df75e1bd3
BlueChip <csBlueChip@gmail.com>
2024-11-20 22:37:40 +0000

    Add Bambu KDF-B

--- MiFare_Docs/Fudan_RF08(S)/bambu-kdf.py
+++ MiFare_Docs/Fudan_RF08(S)/bambu-kdf.py
@@ -14,22 +14,22 @@ def main():
 	if sys.argv[1].lower() == "test":
 		uida = ["5c", "b4", "9c", "a6"]
 		test = [ \
-			bytes.fromhex("8693fc621e12"),  \
-			bytes.fromhex("40a282c042aa"),  \
-			bytes.fromhex("184e3a2e94ca"),  \
-			bytes.fromhex("dd706a9ee951"),  \
-			bytes.fromhex("cd3a76149582"),  \
-			bytes.fromhex("091a165322eb"),  \
-			bytes.fromhex("d0d277bc5c94"),  \
-			bytes.fromhex("cd9edfd14d29"),  \
-			bytes.fromhex("ae46323d85b5"),  \
-			bytes.fromhex("7758739e39b3"),  \
-			bytes.fromhex("f87d61e37cf0"),  \
-			bytes.fromhex("ad44d2c7b55c"),  \
-			bytes.fromhex("f9aa7ada600d"),  \
-			bytes.fromhex("9743553d9ead"),  \
-			bytes.fromhex("ae19cd0a6e4e"),  \
-			bytes.fromhex("a12932a626a7")   \
+			[bytes.fromhex("8693fc621e12"), bytes.fromhex("d538def7b725")], \
+			[bytes.fromhex("40a282c042aa"), bytes.fromhex("f4c0c55efc6a")], \
+			[bytes.fromhex("184e3a2e94ca"), bytes.fromhex("4422b9889125")], \
+			[bytes.fromhex("dd706a9ee951"), bytes.fromhex("5e5b3758858d")], \
+			[bytes.fromhex("cd3a76149582"), bytes.fromhex("51c3d96a0662")], \
+			[bytes.fromhex("091a165322eb"), bytes.fromhex("674f46f05025")], \
+			[bytes.fromhex("d0d277bc5c94"), bytes.fromhex("b48ba097d7b5")], \
+			[bytes.fromhex("cd9edfd14d29"), bytes.fromhex("8c10fbbe4bcf")], \
+			[bytes.fromhex("ae46323d85b5"), bytes.fromhex("92e3f0e07b4c")], \
+			[bytes.fromhex("7758739e39b3"), bytes.fromhex("4f252e68091e")], \
+			[bytes.fromhex("f87d61e37cf0"), bytes.fromhex("4da6db3303e0")], \
+			[bytes.fromhex("ad44d2c7b55c"), bytes.fromhex("37355580af8a")], \
+			[bytes.fromhex("f9aa7ada600d"), bytes.fromhex("0ab78eae4708")], \
+			[bytes.fromhex("9743553d9ead"), bytes.fromhex("96947eab2af6")], \
+			[bytes.fromhex("ae19cd0a6e4e"), bytes.fromhex("1be13ff8cda1")], \
+			[bytes.fromhex("a12932a626a7"), bytes.fromhex("6715bbd7a562")] \
 		]
 
 	elif argc == 4+1:
@@ -61,32 +61,46 @@ def main():
 	print(f"# UID: {uid[0]:02X} {uid[1]:02X} {uid[2]:02X} {uid[3]:02X} ", end='')
 	print(" - Test UID" if 'test' in locals() else "")
 
-	keyA = bambuKDF(uid)
-	if keyA == False:  exit(4)
+	keyA, keyB = bambuKDF(uid)
+	if keyA == False:
+		print("! bambuKDF() failed")
+		exit(4)
 
 	for sec in range(0, 15+1):
-		print(f"  {sec:02d}/A: {keyA[sec].hex()}", end='')
+		print(f"  {sec:2d}/A: {keyA[sec].hex()}", end='')
 		if 'test' in locals():
-			print(" .. OK" if keyA[sec] == test[sec] else " .. Error!")
+			print(" .. OK" if keyA[sec] == test[sec][0] else " .. Error!")
+		else:
+			print("")
+
+	for sec in range(0, 15+1):  # uncomment this line to get KEY-A's before KEY-B's
+		print(f"  {sec:2d}/B: {keyB[sec].hex()}", end='')
+		if 'test' in locals():
+			print(" .. OK" if keyB[sec] == test[sec][1] else " .. Error!")
 		else:
 			print("")
 
 	sys.exit(0)
 
 #+=============================================================================
+# The keys for the hidden sector-formatted area of the 08S card 
+# is a completely separate problem to the Bambu (or, in fact, any) KDF
+#
 def bambuKDF(uid):
 
 	from Cryptodome.Protocol.KDF import HKDF
 	from Cryptodome.Hash         import SHA256
 
 	try:
+		# extracted from Bambu firmware
 		salt = bytes([0x9a,0x75,0x9c,0xf2,0xc4,0xf7,0xca,0xff,0x22,0x2c,0xb9,0x76,0x9b,0x41,0xbc,0x96])
-		keys = HKDF(uid, 6, salt, SHA256, 16, context=b"RFID-A\0")
+		keyA = HKDF(uid, 6, salt, SHA256, 16, context=b"RFID-A\0")
+		keyB = HKDF(uid, 6, salt, SHA256, 16, context=b"RFID-B\0")
 	except Exception as e:
 		print(f"{e}")
-		return False
+		return False, False
 
-	return keys
+	return keyA, keyB
 
 #++============================================================================
 if __name__ == "__main__": main()
